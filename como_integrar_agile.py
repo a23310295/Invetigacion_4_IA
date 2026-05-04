5f5b134ffe46ca6faae1e7e19b17c038ce415eb4
GAELSILLOOOO <a23310295@ceti.mx>
2026-05-03 23:39:13 -0600

    Integracion agile explicacion
    Como se integraria CRISP-DM + AGILE explicacion (ejemplo)

---
+++ como_integrar_agile.py
@@ -0,0 +1,12 @@
+#Ejemplo de Compatibilidad: CRISP-DM + Agile (Scrum)
+#Imagina que una empresa de streaming quiere crear una IA para recomendar películas a sus usuarios. Van a combinar la metodología técnica (CRISP-DM) con la metodología de organización de trabajo (Scrum, que divide el trabajo en bloques de 2 semanas llamados Sprints).
+
+#paso a paso:
+
+#Sprint 1 (Semanas 1 y 2): El equipo aplica los Pasos 1 y 2 de CRISP-DM. Tienen reuniones para entender qué tipo de recomendaciones quieren los directivos (Comprensión del Negocio) y analizan si tienen suficientes bases de datos del historial de visualización de los usuarios (Comprensión de los Datos).
+
+#Sprint 2 (Semanas 3 y 4): Aplican los Pasos 3 y 4 de CRISP-DM. Los programadores limpian las bases de datos y crean un primer modelo de IA básico (así como el código que corriste en Visual Studio) para que empiece a estudiar los gustos de las personas.
+
+#Sprint 3 (Semanas 5 y 6): Llega el Paso 5 de CRISP-DM (Evaluación). Al hacerle el "examen" a la IA, descubren que le está recomendando películas de terror a perfiles infantiles. Como están usando Scrum (que es muy flexible), esta compatibilidad les permite pausar, regresar al paso de los datos para poner filtros de edad y volver a entrenar a la IA rápidamente sin que todo el proyecto colapse.
+
+#Sprint 4 (Semanas 7 y 8): Pasan al Paso 6 de CRISP-DM (Despliegue). La IA ya es precisa y la publican en la aplicación para que un pequeño grupo de usuarios reales la empiece a usar.
